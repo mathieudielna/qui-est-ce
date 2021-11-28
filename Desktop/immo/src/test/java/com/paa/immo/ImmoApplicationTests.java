8aa64a296b0536c449dbdf0323159d43522dee28
Mathieu Dielna <mathieudielna.pro@gmail.com>
2021-11-28 22:09:11 +0100

    init commit

---
+++ Desktop/immo/src/test/java/com/paa/immo/ImmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paa.immo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
